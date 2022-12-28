dae71b2ebaab68371b37214723a00742628f57c3
jguerreno <jguerrenolopez@frba.utn.edu.ar>
2022-12-28 14:24:56 -0300

    refactor(SAMAutomated): abstraccion de logica

--- src/test/java/SAMAutomatedTest.java
+++ src/test/java/SAMAutomatedTest.java
@@ -1,32 +1,20 @@
+import com.FindPage;
 import org.junit.jupiter.api.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 
-import java.time.Duration;
-import java.util.List;
 
 public class SAMAutomatedTest {
     private WebDriver driver;
+    private FindPage page;
 
-    private String palabraBuscada = "devops";
-    private int numeroArticulo = 4;
+    private final String devops = "devops";
+    private final String testManagement = "Test Management";
+    private final int numeroPagina = 2;
+    private final int numeroArticulo = 4;
+    private final int tiempoEspera = 10;
 
-    // Localizador Busqueda
-    private By lupaLocator = By.xpath("//*[@id=\"menu-principal\"]/li[6]/a");
-    private By barraBusquedaLocator = By.id("ocean-search-form-1");
-    // Localizador Paginas de Busqueda
-    private By paginasBusquedaLocator = By.xpath("//a[@class=\"page-numbers\"]");
-    private By pagina2Locator = By.linkText("2");
-    // Localizador Articulo
-    private By articuloLocator = By.xpath("//article[contains(@class,'post-')]");
-    private By ingresoArticuloLocator = By.xpath("//article/div/div[2]/header/h2/a");
-    // Localizador Busqueda
-    private By TextLocator = By.xpath("//span[contains(string(),'Test Management')]");
-    // Localizador Contacto
-    private By contactoLocator = By.xpath("//a[@href=\"https://www.samsistemas.com.ar/contacto/\"][@class=\"menu-link\"]");
-    private By trabajoLocator = By.linkText("Trabaje con nosotros");
-    private By buscarLocator = By.xpath("//*[@id=\"wpcf7-f4996-p101-o1\"]/form/p[4]/input");
     // Localizar Footer
     private By footerLocator = By.xpath("//img[@src=\"https://samsistemas.com.ar/wp-content/uploads/2021/01/sam-icono-site-1.svg\"]");
 
@@ -36,12 +24,14 @@ public class SAMAutomatedTest {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://www.samsistemas.com.ar/");
+
+        page = new FindPage(driver);
+        page.visit("https://www.samsistemas.com.ar/");
     }
 
     @AfterEach
     public void tearDown() {
-        driver.quit();
+        page.quit();
     }
 
 
@@ -49,55 +39,27 @@ public class SAMAutomatedTest {
     @DisplayName("Prueba automatizada Busqueda de Articulos")
     public void testSAMPage() {
         // Busqueda de Palabra
-        driver.findElement(lupaLocator).click();
-
-        WebElement barraBusqueda = driver.findElement(barraBusquedaLocator);
-        barraBusqueda.clear();
-        barraBusqueda.sendKeys(palabraBuscada);
-        barraBusqueda.submit();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        Assertions.assertEquals("Resultados de búsqueda para “devops” – SAM Sistemas", driver.getTitle());
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        page.buscarPalabra(devops);
+        page.waitSeconds(tiempoEspera);
+        Assertions.assertEquals("Resultados de búsqueda para “devops” – SAM Sistemas", page.getTitle());
 
         // Cantidad de Elementos arrojados por la busqueda
-        List<WebElement> articulos = driver.findElements(articuloLocator);
-
-        int cantidadPaginas = driver.findElements(paginasBusquedaLocator).size();
-        for(int i=0 ; i<cantidadPaginas ; i++) {
-            WebElement paginaSiguiente = driver.findElements(paginasBusquedaLocator).get(i);
-            paginaSiguiente.click();
-            articulos.addAll(driver.findElements(articuloLocator));
-        }
-
-        Assertions.assertEquals(articulos.size(), 17);
-        System.out.println("Cantidad de Articulos: " + articulos.size());
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
+        int cantidadElementoBusqueda = page.cantidadElementoBusqueda();
+        Assertions.assertEquals(cantidadElementoBusqueda, 17);
+        System.out.println("Cantidad de Articulos: " + cantidadElementoBusqueda);
+        page.waitSeconds(tiempoEspera);
 
         // Moverse a la página 2 del resultado de búsqueda y clickear sobre el cuarto artículo
-        driver.findElement(pagina2Locator).click();
-        driver.findElements(ingresoArticuloLocator).get(numeroArticulo-1).click();
-
-        Assertions.assertEquals("Automatización de pruebas – SAM Sistemas", driver.getTitle());
-        Assertions.assertTrue(driver.findElements(TextLocator).size() != 0);
-
+        page.articlePage(numeroPagina, numeroArticulo);
+        Assertions.assertEquals("Automatización de pruebas – SAM Sistemas", page.getTitle());
+        Assertions.assertTrue(page.wordExists(testManagement));
 
         // Seleccionar -> Contacto -> “Trabaje con nosotros”
-        Actions action = new Actions(driver);
-        WebElement contacto = driver.findElement(contactoLocator);
-        action.moveToElement(contacto).perform();
+        page.trabajeConNosotros();
+        Assertions.assertTrue(page.botonEnvioFormTrabajoDisponible());
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        WebElement trabajo = driver.findElement(trabajoLocator);
-        action.moveToElement(trabajo).click().perform();
-        Assertions.assertTrue(driver.findElement(buscarLocator).isEnabled());
-        ((JavascriptExecutor)driver).executeScript("window.scrollTo(0, document.body.scrollHeight);");
-        Assertions.assertTrue(driver.findElement(footerLocator).isDisplayed());
+        // Voy al footer
+        page.moveDown();
+        Assertions.assertTrue(page.isDisplayed(footerLocator));
     }
 }
